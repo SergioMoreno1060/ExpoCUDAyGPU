0faaa00f55151be93fd053a04d686fafbe9f6d42
SergioMoreno1060 <128836484+SergioMoreno1060@users.noreply.github.com>
2024-10-02 15:57:24 -0500

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from streamlit_components_react import ReactComponent
 st.title("CUDA y GPU: Presentación")
 
 # Cargar el componente React
-presentation_cards = ReactComponent("PresentationCards.jsx")
+presentation_cards = ReactComponent("presentation-cards-component.tsx")
 
 # Renderizar el componente
 presentation_cards()
